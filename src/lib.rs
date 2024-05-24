b8405df0f45af8ce83a9f44ea791ca81b4d8fd19
Masterchef365 <duncan.freeman1@gmail.com>
2024-05-23 23:50:39 -0700

    Start hooking things up in the skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,15 @@ pub struct SolverConfig {
     dt: f32,
 }
 
-/// Total memory is of order O(width^dimensions)
+/// Total memory is of order O(width^(N+1))
+/// Where N is the number of cell dimensions (3D -> N=3)
 ///
-/// The first dimension of the Array corresponds to the component of the vector field.
-/// So x corresponds to (0, ... ) and y corresponds to (1, ... ) and so on.
-/// The remaining dimensions are the actual interpolation grid.
+/// The last dimension of the Array corresponds to the component of the vector field.
+/// So x corresponds to {N entries}, 0) and y corresponds to ({N entries}, 1) and so on up to ({N entries}, N) 
+/// 
+/// note: {N entries} can be any rectangular N-dimensional shape, 
+/// but the last array dimension is always N
+///
 /// So the Array will have a shape with size (dimensions + 1) 
 #[derive(Clone)]
 pub struct FlowField(Array<f32, IxDyn>);
@@ -18,7 +22,7 @@ pub struct FluidSolver {
     flow: FlowField,
 }
 
-/// This is a two-dimensional array for the point cloud, where the first dimension corresponds
+/// This is a two-dimensional array for the point cloud, where the last dimension corresponds
 /// to the coponent of the vector, and the second dimension corresponds to which point is selected
 #[derive(Clone)]
 pub struct PointCloud(Array2<f32>);
@@ -32,11 +36,11 @@ impl FluidSolver {
         todo!()
     }
 
-    pub fn flow(&self) -> &FlowField {
+    pub fn get_flow(&self) -> &FlowField {
         &self.flow
     }
 
-    pub fn flow_mut(&mut self) -> &mut FlowField {
+    pub fn get_flow_mut(&mut self) -> &mut FlowField {
         &mut self.flow
     }
 }
@@ -47,7 +51,11 @@ pub fn sweep_pointcloud(pcld: &mut PointCloud, flow: &FlowField, dt: f32) {
 }
 
 impl FlowField {
-    fn new(dimensions: usize, width: usize) -> Self {
+    fn from_width(dimensions: usize, width: usize) -> Self {
+        let mut arr_dimensions = vec![width; dimensions];
+        // The last dimension implies a N-dimensional vector for each Cell.
+        arr_dimensions.push(dimensions);
+
         Self(Array::zeros(vec![width; dimensions + 1]))
     }
 }
@@ -57,4 +65,5 @@ impl PointCloud {
         Self(Array::zeros((dimensions, 0)))
     }
 }
+
 
