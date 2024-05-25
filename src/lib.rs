6c9d00776b25eee6198da58c372da102eee49fed
Masterchef365 <duncan.freeman1@gmail.com>
2024-05-25 01:21:22 -0700

    Crazy iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use ndarray::{Array, Array2, IxDyn};
+use std::ops::{Index, IndexMut};
+
+use ndarray::{Array, Array2, ArrayView, IxDyn};
 
 #[derive(Clone)]
 pub struct SolverConfig {
@@ -9,12 +11,12 @@ pub struct SolverConfig {
 /// Where N is the number of cell dimensions (3D -> N=3)
 ///
 /// The last dimension of the Array corresponds to the component of the vector field.
-/// So x corresponds to {N entries}, 0) and y corresponds to ({N entries}, 1) and so on up to ({N entries}, N) 
-/// 
-/// note: {N entries} can be any rectangular N-dimensional shape, 
+/// So x corresponds to {N entries}, 0) and y corresponds to ({N entries}, 1) and so on up to ({N entries}, N)
+///
+/// note: {N entries} can be any rectangular N-dimensional shape,
 /// but the last array dimension is always N
 ///
-/// So the Array will have a shape with size (dimensions + 1) 
+/// So the Array will have a shape with size (dimensions + 1)
 #[derive(Clone)]
 pub struct FlowField(Array<f32, IxDyn>);
 
@@ -43,6 +45,13 @@ impl FluidSolver {
     pub fn get_flow_mut(&mut self) -> &mut FlowField {
         &mut self.flow
     }
+
+    /// Number of dimensions
+    pub fn dims(&self) -> usize {
+        self.flow.dims()
+    }
+
+    fn advect(&mut self, config: &SolverConfig) {}
 }
 
 /// Sweeps the given point cloud along the given flow field
@@ -58,6 +67,37 @@ impl FlowField {
 
         Self(Array::zeros(vec![width; dimensions + 1]))
     }
+
+    fn dims(&self) -> usize {
+        self.0.ndim() - 1
+    }
+
+    /// Returns an iterator over the cell positions as an integer
+    fn enumerate(&self) -> Box<dyn Iterator<Item = FatVector<usize>> + '_> {
+        self.enumerate_rec(0, FatVector::from_size(self.dims()))
+    }
+
+    fn enumerate_rec(
+        &self,
+        idx: usize,
+        fat: FatVector<usize>,
+    ) -> Box<dyn Iterator<Item = FatVector<usize>> + '_> {
+        let num_indices = self.0.shape().get(idx).copied().unwrap_or(0);
+        let mut fat = fat;
+        Box::new(
+            (0..num_indices)
+                .map(move |j| {
+                    fat[idx] = j;
+                    self.enumerate_rec(idx + 1, fat)
+                })
+                .flatten(),
+        )
+    }
+
+    /// Bilinear interpolation at the given position in ND space
+    fn bilinear(position: FatVector<f32>) -> f32 {
+        todo!()
+    }
 }
 
 impl PointCloud {
@@ -66,4 +106,28 @@ impl PointCloud {
     }
 }
 
+/// Assume we aren't using more than this many dimensions
+const FAT: usize = 24;
 
+/// The typical (size, array) tuple setup
+#[derive(Clone, Copy, Debug)]
+pub struct FatVector<T>(usize, [T; FAT]);
+
+impl<T: Default + Copy> FatVector<T> {
+    fn from_size(size: usize) -> Self {
+        FatVector(size, [T::default(); FAT])
+    }
+}
+
+impl<T> Index<usize> for FatVector<T> {
+    type Output = T;
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.1[index]
+    }
+}
+
+impl<T> IndexMut<usize> for FatVector<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.1[index]
+    }
+}
