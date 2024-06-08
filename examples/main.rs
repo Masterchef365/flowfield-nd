d910c0cefe39a017535372032d636e87330d94ec
Masterchef365 <duncan.freeman1@gmail.com>
2024-06-08 00:42:05 -0700

    The funny test le algorithm

--- examples/main.rs
+++ examples/main.rs
@@ -1,7 +1,7 @@
 use flowfield_nd::FlowField;
 
 fn main() {
-    let ff = FlowField::new(2, 10);
+    let ff = FlowField::new(3, 10);
     for x in ff.enumerate() {
         dbg!(x);
     }
@@ -9,5 +9,9 @@ fn main() {
 
     for x in flowfield_nd::neighborhood(3) {
         println!("{:?}", x);
+    }
+
+    for i in 0..10 {
+        dbg!(i, flowfield_nd::neighborhood(i).len());
     }
 }
